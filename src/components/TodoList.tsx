01b7d168f40309975cd62dbed9a05622087f6515
Livecan <roman.meszaros@gmail.com>
2024-06-30 08:30:04 +0200

    STYLE TodoItem cards

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -2,22 +2,35 @@
 import { useContext } from "react";
 import { ListDataContext } from "./context/ListDataContext";
 import { Button } from "./ui/Button";
+import { Card, CardContent, CardFooter, CardHeader } from "./ui/card";
+import { CheckIcon, Trash2Icon } from "lucide-react";
 
 export default function TodoList() {
   const { list, editItem, removeItem } = useContext(ListDataContext);
 
   return (
     <>
-      <ul>
+      <ul className="space-y-2">
         {list.map((item: { id: string; name: string; dueDate: Date }) => (
           <li key={item.id}>
-            {item.name} - {item.dueDate?.toDateString?.()}
-            <Button
-              onClick={() => editItem(item.id, { completedDate: new Date() })}
-            >
-              Complete
-            </Button>
-            <Button onClick={() => removeItem(item.id)}>Delete</Button>
+            <Card>
+              <CardHeader className="py-2">{item.name}</CardHeader>
+              <CardContent className="py-2 justify-end">
+                {item.dueDate?.toDateString?.()}
+              </CardContent>
+              <CardFooter className="p-2 justify-end space-x-2">
+                <Button
+                  onClick={() =>
+                    editItem(item.id, { completedDate: new Date() })
+                  }
+                >
+                  <CheckIcon />
+                </Button>
+                <Button onClick={() => removeItem(item.id)}>
+                  <Trash2Icon />
+                </Button>
+              </CardFooter>
+            </Card>
           </li>
         ))}
       </ul>
